39e6cbd656db40fbc87304bb0b741df000a4f06a
vh <vh@socprime.com>
2023-11-09 16:45:18 +0200

    Clean Microsoft Sentinel functions

--- siem-converter/app/converter/backends/microsoft/siem_functions/base.py
+++ siem-converter/app/converter/backends/microsoft/siem_functions/base.py
@@ -1,34 +1,8 @@
-from app.converter.backends.microsoft.siem_functions.aggregation import AlaAggregationFunctionRender
-from app.converter.backends.microsoft.siem_functions.search import AlaSearchFunctionRender
-from app.converter.backends.microsoft.siem_functions.sort import AlaSortFunctionRender
-from app.converter.backends.microsoft.siem_functions.table import AlaTableFunctionRender
 from app.converter.core.functions import Functions
-from app.converter.core.models.functions.aggregation import AggregationExpression
-from app.converter.core.models.functions.search import SearchExpression
-from app.converter.core.models.functions.sort import SortExpression
-from app.converter.core.models.functions.table import TableExpression
 from app.converter.core.models.functions.types import ParsedFunctions, NotSupportedFunction
 
 
 class MicroSoftQueryFunctions(Functions):
-
-    render_functions_map = {
-        SortExpression: AlaSortFunctionRender, 
-        SearchExpression: AlaSearchFunctionRender,
-        AggregationExpression: AlaAggregationFunctionRender,
-        TableExpression: AlaTableFunctionRender,
-        # WhereExpression: AlaWhereFunctionRender
-    }
-
-    def render(self, functions: list):
-        query = "| "
-        funcs = []
-        for function in functions:
-            if render_class := self.render_functions_map.get(type(function)):
-                funcs.append(render_class(function).render())
-        query += " | ".join(funcs)
-        query = query.rstrip(" ")
-        return query
 
     def parse(self, query: str):
         result = []
