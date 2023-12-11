3dc315d0fe53cf7ee37e4d41721b310d3b4b621d
dmytro.tarnopolskyi <dmytro.tarnopolskyi@socprime.com>
2023-12-11 10:30:40 +0100

    update crowdstrike platform title

--- translator/app/translator/platforms/crowdstrike/const.py
+++ translator/app/translator/platforms/crowdstrike/const.py
@@ -2,10 +2,10 @@ from app.translator.core.models.platform_details import PlatformDetails
 
 CROWDSTRIKE_QUERY_DETAILS = {
     "siem_type": "crowdstrike-spl-query",
-    "name": "CrowdStrike",
+    "name": "CrowdStrike Endpoint Security",
     "platform_name": "Query (SPL)",
     "group_id": "crowdstrike",
-    "group_name": "CrowdStrike"
+    "group_name": "CrowdStrike Endpoint Security"
 }
 
 crowdstrike_query_details = PlatformDetails(**CROWDSTRIKE_QUERY_DETAILS)
