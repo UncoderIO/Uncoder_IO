a844da8c6583bed3483d3b192aeaa628265a8fd2
Oleksandr Volha <oleksandr.volha@socprime.com>
2024-02-05 11:29:06 +0200

    os and splunk signature fix

--- translator/app/translator/platforms/opensearch/renders/opensearch_rule.py
+++ translator/app/translator/platforms/opensearch/renders/opensearch_rule.py
@@ -54,6 +54,8 @@ class OpenSearchRuleRender(OpenSearchQueryRender):
         meta_info: Optional[MetaInfoContainer] = None,
         source_mapping: Optional[SourceMapping] = None,  # noqa: ARG002
         not_supported_functions: Optional[list] = None,
+        *args,  # noqa: ARG002
+        **kwargs,  # noqa: ARG002
     ) -> str:
         query = super().finalize_query(prefix=prefix, query=query, functions=functions)
         rule = copy.deepcopy(OPENSEARCH_RULE)
