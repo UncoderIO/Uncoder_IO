2167b49775bda9fc8f151b566edc263878db751b
Oleksandr Volha <alexandervolha@gmail.com>
2024-03-20 11:44:14 +0200

    fix sentinel kql verbatim str parsing

---
+++ uncoder-core/app/translator/platforms/microsoft/custom_types/values.py
@@ -0,0 +1,6 @@
+from app.translator.core.custom_types.values import ValueType
+
+
+class MicrosoftValueType(ValueType):
+    verbatim_double_quotes_value = "v_d_q_value"
+    verbatim_single_quotes_value = "v_s_q_value"
