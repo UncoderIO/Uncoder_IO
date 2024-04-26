f27cf59fa818182f7a01f8d473ff26c9a4668028
Oleksandr Volha <alexandervolha@gmail.com>
2024-04-26 17:28:18 +0300

    Merge remote-tracking branch 'origin' into gis-7649

--- uncoder-core/app/translator/platforms/__init__.py
+++ uncoder-core/app/translator/platforms/__init__.py
@@ -1,3 +1,4 @@
+from app.translator.platforms.arcsight.renders.arcsight_cti import ArcsightKeyword
 from app.translator.platforms.athena.parsers.athena import AthenaQueryParser
 from app.translator.platforms.athena.renders.athena import AthenaQueryRender
 from app.translator.platforms.athena.renders.athena_cti import AthenaCTI
@@ -41,9 +42,9 @@ from app.translator.platforms.microsoft.renders.microsoft_sentinel_cti import Mi
 from app.translator.platforms.microsoft.renders.microsoft_sentinel_rule import MicrosoftSentinelRuleRender
 from app.translator.platforms.opensearch.parsers.opensearch import OpenSearchQueryParser
 from app.translator.platforms.opensearch.renders.opensearch import OpenSearchQueryRender
-from app.translator.platforms.palo_alto.renders.cortex_xsiam import CortexXQLQueryRender
 from app.translator.platforms.opensearch.renders.opensearch_cti import OpenSearchCTI
 from app.translator.platforms.opensearch.renders.opensearch_rule import OpenSearchRuleRender
+from app.translator.platforms.palo_alto.renders.cortex_xsiam import CortexXQLQueryRender
 from app.translator.platforms.qradar.parsers.qradar import QradarQueryParser
 from app.translator.platforms.qradar.renders.qradar import QradarQueryRender
 from app.translator.platforms.qradar.renders.qradar_cti import QRadarCTI
@@ -115,6 +116,7 @@ __ALL_RENDERS_CTI = (
     MicrosoftDefenderCTI(),
     QRadarCTI(),
     SplunkCTI(),
+    ArcsightKeyword(),
     ChronicleQueryCTI(),
     CrowdStrikeCTI(),
     SumologicCTI(),
