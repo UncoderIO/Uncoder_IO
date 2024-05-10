15bd5792ba48f1f26e75b59bd3be2a817641972b
oleksandr.volha <oleksandr.volha@socprime.com>
2024-05-10 11:40:31 +0300

    add license

--- uncoder-core/app/translator/platforms/qualys/renders/qualys_cti.py
+++ uncoder-core/app/translator/platforms/qualys/renders/qualys_cti.py
@@ -1,3 +1,19 @@
+"""
+Uncoder IO Community Edition License
+-----------------------------------------------------------------
+Copyright (c) 2024 SOC Prime, Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-----------------------------------------------------------------
+"""
+
 from app.translator.core.models.platform_details import PlatformDetails
 from app.translator.core.render_cti import RenderCTI
 from app.translator.managers import render_cti_manager
