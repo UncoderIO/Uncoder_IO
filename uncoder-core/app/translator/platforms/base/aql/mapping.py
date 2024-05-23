cdaeab5c07b8160ef59e232d317f8a86adf45ce4
Viktor Hrebeniuk <76157115+saltar-ua@users.noreply.github.com>
2024-05-23 16:59:16 +0300

    Fix AQL mapping logic for default mapping

--- uncoder-core/app/translator/platforms/base/aql/mapping.py
+++ uncoder-core/app/translator/platforms/base/aql/mapping.py
@@ -77,6 +77,8 @@ class AQLMappings(BasePlatformMappings):
 
         if not suitable_source_mappings:
             for source_mapping in self._source_mappings.values():
+                if source_mapping.source_id == DEFAULT_MAPPING_NAME:
+                    continue
                 if source_mapping.fields_mapping.is_suitable(field_names):
                     suitable_source_mappings.append(source_mapping)
 
