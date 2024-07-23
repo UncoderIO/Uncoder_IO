8fac449330f92fce1db91a368bab7c3ce2ae953e
Dmytro <47281757+tarnopolskyi@users.noreply.github.com>
2024-07-23 16:15:24 +0200

    update render

--- uncoder-core/app/translator/core/render.py
+++ uncoder-core/app/translator/core/render.py
@@ -372,13 +372,15 @@ class PlatformQueryRender(QueryRender):
 
         return result
 
-    def _get_source_mappings(self, source_mapping_ids: list[str]) -> list[SourceMapping]:
+    def _get_source_mappings(
+        self, source_mapping_ids: list[str], return_default: bool = True
+    ) -> Optional[list[SourceMapping]]:
         source_mappings = []
         for source_mapping_id in source_mapping_ids:
             if source_mapping := self.mappings.get_source_mapping(source_mapping_id):
                 source_mappings.append(source_mapping)
 
-        if not source_mappings:
+        if return_default and not source_mappings:
             source_mappings = [self.mappings.get_source_mapping(DEFAULT_MAPPING_NAME)]
 
         return source_mappings
