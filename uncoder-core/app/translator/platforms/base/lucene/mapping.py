a8b672d4c20391263c74334f2fa41417177e483c
Viktor Hrebeniuk <76157115+saltar-ua@users.noreply.github.com>
2024-07-25 17:13:26 +0300

    Improve roota render + lucene mapping

--- uncoder-core/app/translator/platforms/base/lucene/mapping.py
+++ uncoder-core/app/translator/platforms/base/lucene/mapping.py
@@ -36,8 +36,13 @@ class LuceneMappings(BasePlatformMappings):
             if index and log_source_signature.is_suitable(index=index):
                 if source_mapping.fields_mapping.is_suitable(field_names):
                     suitable_source_mappings.append(source_mapping)
-            elif source_mapping.fields_mapping.is_suitable(field_names):
-                suitable_source_mappings.append(source_mapping)
+
+        if not suitable_source_mappings:
+            for source_mapping in self._source_mappings.values():
+                if source_mapping.source_id == DEFAULT_MAPPING_NAME:
+                    continue
+                if source_mapping.fields_mapping.is_suitable(field_names):
+                    suitable_source_mappings.append(source_mapping)
 
         if not suitable_source_mappings:
             suitable_source_mappings = [self._source_mappings[DEFAULT_MAPPING_NAME]]
