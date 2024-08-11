69f8f6cc19bd89abe109d982356b2e3abb935c22
oleksandr.volha <oleksandr.volha@socprime.com>
2024-08-11 20:50:13 +0300

    spl str value manager

--- uncoder-core/app/translator/platforms/base/spl/escape_manager.py
+++ uncoder-core/app/translator/platforms/base/spl/escape_manager.py
@@ -6,9 +6,7 @@ from app.translator.core.models.escape_details import EscapeDetails
 
 
 class SplEscapeManager(EscapeManager):
-    escape_map: ClassVar[dict[str, list[EscapeDetails]]] = {
-        ValueType.value: [EscapeDetails(pattern='("|(?<!\\\\)\\\\(?![*?\\\\]))')]
-    }
+    escape_map: ClassVar[dict[str, list[EscapeDetails]]] = {ValueType.value: [EscapeDetails(pattern=r"([<>=\"'\|\\])")]}
 
 
 spl_escape_manager = SplEscapeManager()
