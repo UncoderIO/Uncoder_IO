e55a7dfd18d9409a61b194428382f273f418460c
Gesyk Nazar <77268518+nazargesyk@users.noreply.github.com>
2024-10-08 15:28:57 +0300

    gis-83825 change sentinel one const

--- uncoder-core/app/translator/platforms/sentinel_one/const.py
+++ uncoder-core/app/translator/platforms/sentinel_one/const.py
@@ -1,7 +1,20 @@
+from app.translator.core.models.platform_details import PlatformDetails
+
+
+PLATFORM_DETAILS = {"group_id": "sentinel-one", "group_name": "SentinelOne"}
+
 SENTINEL_ONE_EVENTS_QUERY_DETAILS = {
     "platform_id": "s1-events",
     "name": "SentinelOne Events Query",
-    "group_name": "SentinelOne",
-    "group_id": "sentinel-one",
     "platform_name": "Query (Events)",
+    **PLATFORM_DETAILS,
 }
+
+SENTINEL_ONE_POWER_QUERY_DETAILS = {
+    "platform_id": "sentinel-one-power-query",
+    "name": "SentinelOne Power Query",
+    "platform_name": "Power Query",
+    **PLATFORM_DETAILS,
+}
+
+sentinel_one_events_query_details = PlatformDetails(**SENTINEL_ONE_EVENTS_QUERY_DETAILS)
