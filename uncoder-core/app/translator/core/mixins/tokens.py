32814a1c770c414c2d5d864594c5cb8992a126a2
Gesyk Nazar <77268518+nazargesyk@users.noreply.github.com>
2024-11-04 11:29:13 +0200

    gis-8971 fixes

--- uncoder-core/app/translator/core/mixins/tokens.py
+++ uncoder-core/app/translator/core/mixins/tokens.py
@@ -1,14 +1,14 @@
 from typing import Union
 
+from app.translator.core.const import QUERY_TOKEN_TYPE
 from app.translator.core.custom_types.tokens import LogicalOperatorType, OperatorType
 from app.translator.core.mapping import SourceMapping
 from app.translator.core.models.query_tokens.field_value import FieldValue
 from app.translator.core.models.query_tokens.identifier import Identifier
-from app.translator.core.models.query_tokens.value import Value
 
 
 class ExtraConditionMixin:
-    def generate_extra_conditions(self, source_mapping: SourceMapping) -> list[Union[Value, Identifier]]:
+    def generate_extra_conditions(self, source_mapping: SourceMapping) -> list[QUERY_TOKEN_TYPE]:
         extra_tokens = []
         for field, value in source_mapping.conditions.items():
             extra_tokens.extend(
