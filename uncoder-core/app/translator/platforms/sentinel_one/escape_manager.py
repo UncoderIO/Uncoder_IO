26e468c7eb3c0dd95f71997b8c906ecee37ab657
Gesyk Nazar <77268518+nazargesyk@users.noreply.github.com>
2024-11-20 14:44:56 +0200

    gis-9195 fixes

--- uncoder-core/app/translator/platforms/sentinel_one/escape_manager.py
+++ uncoder-core/app/translator/platforms/sentinel_one/escape_manager.py
@@ -9,7 +9,7 @@ from app.translator.platforms.sentinel_one.custom_types.values import SentinelOn
 class SentinelOnePowerQueryEscapeManager(EscapeManager):
     escape_map: ClassVar[dict[str, list[EscapeDetails]]] = {
         ValueType.value: [EscapeDetails(pattern=r"\\", escape_symbols=r"\\\\")],
-        ValueType.regex_value: [EscapeDetails(pattern=r"([$^*+()\[\]{}|.?\-\\])", escape_symbols=r"\\\\\\\\")],
+        ValueType.regex_value: [EscapeDetails(pattern=r"([$^*+()\[\]{}|.?\-\\])", escape_symbols=r"\\\1")],
         SentinelOneValueType.double_escape_regex_value: [EscapeDetails(pattern=r"\\", escape_symbols=r"\\\\")],
     }
 
