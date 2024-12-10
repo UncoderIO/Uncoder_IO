9186e6448a3643e7023430824fc2624a00b159cf
Gesyk Nazar <77268518+nazargesyk@users.noreply.github.com>
2024-12-10 15:25:11 +0200

    gis-9241 fix ArcsightKeyword cti

--- uncoder-core/app/translator/platforms/arcsight/renders/arcsight_cti.py
+++ uncoder-core/app/translator/platforms/arcsight/renders/arcsight_cti.py
@@ -1,16 +1,16 @@
 from app.translator.core.models.platform_details import PlatformDetails
 from app.translator.core.render_cti import RenderCTI
 from app.translator.managers import render_cti_manager
-from app.translator.platforms.arcsight.const import ARCSIGHT_QUERY_DETAILS
+from app.translator.platforms.arcsight.const import arcsight_query_details
 from app.translator.platforms.arcsight.mappings.arcsight_cti import DEFAULT_ARCSIGHT_MAPPING
 
 
 @render_cti_manager.register
 class ArcsightKeyword(RenderCTI):
-    details: PlatformDetails = PlatformDetails(**ARCSIGHT_QUERY_DETAILS)
+    details: PlatformDetails = arcsight_query_details
 
     default_mapping = DEFAULT_ARCSIGHT_MAPPING
-    field_value_template: str = "{key} = {value}"
+    field_value_template: str = '{key} = "{value}"'
     or_operator: str = " OR "
     group_or_operator: str = " OR "
     or_group: str = "{or_group}"
