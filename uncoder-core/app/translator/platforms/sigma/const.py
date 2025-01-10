58aa2543309d2e0defe0707307c523cbd621771a
Gesyk Nazar <77268518+nazargesyk@users.noreply.github.com>
2025-01-10 10:10:22 +0200

    gis-9379 change sigma cti mapping

--- uncoder-core/app/translator/platforms/sigma/const.py
+++ uncoder-core/app/translator/platforms/sigma/const.py
@@ -8,4 +8,16 @@ SIGMA_RULE_DETAILS = {
     "group_id": "sigma",
 }
 
+DEFAULT_SIGMA_CTI_MAPPING = {
+    "SourceIP": "SourceIP",
+    "DestinationIP": "DestinationIP",
+    "Domain": "Domain",
+    "URL": "URL",
+    "HashMd5": "HashMd5",
+    "HashSha1": "HashSha1",
+    "HashSha256": "HashSha256",
+    "HashSha512": "HashSha512",
+}
+
+
 sigma_rule_details = PlatformDetails(**SIGMA_RULE_DETAILS)
